87b7aa441d7effa3c12ab1ebf40e528c09343c53
ChunkyMitts <15160009@studentmail.ul.ie>
2019-04-22 23:35:29 +0100

    Added user visitor implementation

--- src/main/java/account/CancellationsVisitor.java
+++ src/main/java/account/CancellationsVisitor.java
@@ -3,6 +3,8 @@ package account;
 import interceptor.LoggingContext;
 import interceptor.LoggingDispatcher;
 
+import backgroundServices.API_Handlers.insertRequestHandler;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,7 +12,34 @@ public class CancellationsVisitor implements UserVisitor {
 
     @Override
     public void visit(Customer user) {
-
+        Scanner in = new Scanner(System.in);
+        if(user.hasBookings()) {
+            boolean end = false;
+            System.out.println("Enter Flight ID of flight to be cancelled or e/E to exit: ");
+            do {
+                String input = in.nextLine();
+                if(input.matches("e") || input.matches("E"))
+                    end = true;
+                else {
+                    try {
+                        int bookingID = Integer.parseInt(input);
+                        if (bookingID !=0) {
+                            new insertRequestHandler().disableBookingByID(bookingID);
+                            LoggingDispatcher.getInstanceOfDispatcher().accountChange(createUserContext(user,
+                                    bookingID));
+                            System.out.println("Booking canceled");
+                            end = true;
+                        } else {
+                            System.out.println("Incorrect ID entered");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Wrong input Please enter a number");
+                    }
+                }
+            } while (!end);
+        } else {
+            System.out.println("no bookings");
+        }
     }
 
     @Override
@@ -30,7 +59,7 @@ public class CancellationsVisitor implements UserVisitor {
                     ArrayList<Integer> flightNums = user.getFlightsNumbers();
                     if (flightNums.contains(flightID)) {
                         //execute delete
-                        LoggingDispatcher.getInstanceOfDispatcher().accountChange(createContext(user, flightID));
+                        LoggingDispatcher.getInstanceOfDispatcher().accountChange(createAirlineContext(user, flightID));
                         System.out.println("Flight canceled");
                         end = true;
                     } else {
@@ -43,9 +72,15 @@ public class CancellationsVisitor implements UserVisitor {
         } while (!end);
     }
 
-    private LoggingContext createContext(AirlineEmployee user, int flightID) {
+    private LoggingContext createAirlineContext(AirlineEmployee user, int flightID) {
         LoggingContext context = new LoggingContext(user.getUserID(), "Flight canceled", user.getUserName(),
                 user.getAirlineID(), flightID);
+        return context;
+    }
+
+    private LoggingContext createUserContext(Customer user, int bookingID) {
+        LoggingContext context = new LoggingContext(user.getUserID(), "bookings canceled", user.getUserName(),
+                user.getUserID(), bookingID);
         return context;
     }
 
--- src/main/java/account/Customer.java
+++ src/main/java/account/Customer.java
@@ -1,8 +1,12 @@
 package account;
 
+import backgroundServices.API_Handlers.getRequestHandler;
+import org.json.JSONObject;
+
 public class Customer extends User implements UserVisitable {
 
     private LoyaltyPoints points;
+    private JSONObject[] bookings;
 
     public Customer(String userName, int userID, int userType, int loyaltyPoints){
         this.setUserName(userName);
@@ -18,5 +22,21 @@ public class Customer extends User implements UserVisitable {
     @Override
     public void accept(UserVisitor visitor) {
         visitor.visit(this);
+    }
+
+    public void showBookings() {
+        for(JSONObject i : bookings)
+        System.out.println(i);
+    }
+
+    public boolean hasBookings(){
+        getRequestHandler dbPullHandler = new getRequestHandler();
+        dbPullHandler.getBookingByID(getUserID());
+        if(dbPullHandler.getApiResponseResults() == null)
+            return false;
+        else {
+            bookings = dbPullHandler.getApiResponseResults();
+            return true;
+        }
     }
 }
