8e028421af96dc2637e363c95f1849e1b0ac5a08
William <willoleary6@gmail.com>
2019-04-23 11:01:12 +0100

    final clean up

--- src/main/java/TDD.java
+++
@@ -1,9 +0,0 @@
-public class TDD {
-    public static void main (String [] args){
-        System.out.println(getText());
-    }
-
-    public static String getText(){
-        return "Hello World";
-    }
-}
